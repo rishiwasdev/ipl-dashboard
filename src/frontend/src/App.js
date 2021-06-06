05db5c0c554a04b356cf7b7641b04e6b3bdaa66f
rwasd <rishi.wasdev@cointribe.com>
2021-06-06 21:00:15 +0530

    Add Match page in React App

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/teams/:teamName/matches/:year">
-            <MatchPage/>
+            <MatchPage />
           </Route>
           <Route path="/teams/:teamName">
             <TeamPage />
--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,8 +1,32 @@
+import { React, useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { useParams } from 'react-router-dom';
 
 export const MatchPage = () => {
-    return (
-        <div className="MatchPage">
-            <h1>Match Page</h1>
-        </div>
-    )
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+
+  useEffect(
+    () => {
+      const fetchMatches = async () => {
+        const response = await fetch(`http://localhost:8080/teams/${teamName}/matches?year=${year}`);
+        const data = await response.json();
+        setMatches(data);
+      }
+      fetchMatches();
+    }, [] // without array [], it keeps calling API
+    );
+
+  if (!matches || matches.length === 0) {
+    return <h1>Matches detail not found</h1>;
+  }
+
+  return (
+    <div className="MatchPage">
+      <h1>Match Page</h1>
+      {
+        matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
+      }
+    </div>
+  );
 }
